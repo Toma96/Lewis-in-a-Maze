0ea4988350961dd8118dc5b183496e46a0586284
Toma96 <60894511+Toma96@users.noreply.github.com>
2020-02-12 14:05:05 +0100

    Update databases.py

--- databases.py
+++ databases.py
@@ -1,12 +1,14 @@
 import sqlite3
 
-con = sqlite3.connect("Highscores.db")
-cursor = con.cursor()
-queryTable = "CREATE TABLE Highscores (" \
-             "ID INTEGER PRIMARY KEY AUTOINCREMENT, " \
-             "Ime TEXT, " \
-             "Points INTEGER )"
 
-cursor.execute(queryTable)
-con.commit()
-con.close()
+if __name__=='__main__':
+    con = sqlite3.connect("Highscores.db")
+    cursor = con.cursor()
+    queryTable = "CREATE TABLE Highscores (" \
+                 "ID INTEGER PRIMARY KEY AUTOINCREMENT, " \
+                 "Ime TEXT, " \
+                 "Points INTEGER )"
+
+    cursor.execute(queryTable)
+    con.commit()
+    con.close()
